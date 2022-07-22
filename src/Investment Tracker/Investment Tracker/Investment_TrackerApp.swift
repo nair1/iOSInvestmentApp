8a0d98e4d28f06361a89eb3dd99e66bf0b97473c
Sachin Nair <sachinn1118@gmail.com>
2022-07-22 15:48:33 -0500

    Added gitignore file, added base project files

---
+++ src/Investment Tracker/Investment Tracker/Investment_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Investment_TrackerApp.swift
+//  Investment Tracker
+//
+//  Created by Sachin Nair on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Investment_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
